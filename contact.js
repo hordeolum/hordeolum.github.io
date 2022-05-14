47a927f70e48bae95fde91cddef4984052d9b033
hordeolum <eyal.gur888@gmail.com>
2022-05-14 16:24:05 +0300

    Myopia prevention
    Myopia prevention

---
+++ contact.js
@@ -0,0 +1,13 @@
+const names = document.getElementById("name")
+const email = document.getElementById("email")
+const subject = document.getElementById("subject")
+const msg = document.getElementById("msg")
+
+if (names.value === " " || names.value === null ) {
+    let messages = []
+    messages.push("Name is required")
+}
+if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.inn
+}
